548b81b4c25d99f94f5623413cf359f6b78224fc
Mikhail Krivushin <krivushinme@gmail.com>
2014-04-16 16:33:24 +0400

    Added dump to errors

--- lib/trafaret/errors.rb
+++ lib/trafaret/errors.rb
@@ -9,5 +9,16 @@ module Trafaret
     def inspect
       "#<Trafaret::Error(#{message.inspect})>"
     end
+
+    def dump
+      case @message
+      when ::Hash
+        Hash[@message.map { |k, v| [k, v.dump] }]
+      when ::Array
+        @message.map &:dump
+      else
+        @message
+      end
+    end
   end
 end
